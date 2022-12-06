aa0d967c7e8172f5d46879832249a9c8d5a228dd
Bautista Insua <bautiinsua1210@gmail.com>
2022-12-05 21:09:26 -0300

    new feature template

--- app_bautista_insua/app/config/urls.py
+++ app_bautista_insua/app/config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from app.views.family_member import members
+from app.views.index import index
+
 urlpatterns = [
+    path('', index),
     path('family/', members),
 ]
